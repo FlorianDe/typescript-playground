b79a97089033e00e7c2e7d5a5b0341ba85d53d87
FlorianDe <FlorianDe@users.noreply.github.com>
2024-12-17 23:28:46 +0100

    Added basic tcp-client and tcp-echo server example

---
+++ src/network/protocols/tcp/tcp-client.ts
@@ -0,0 +1,25 @@
+import * as net from 'net';
+
+const client = net.createConnection(8080, 'localhost', () => {
+	console.log('Connected to server');
+
+	client.on('data', (data) => {
+		console.log('Received data:\n\t', data.toString());
+	});
+
+	client.on('error', (error) => {
+		console.error('Socket Error', error);
+	});
+
+	client.on('close', () => {
+		console.log('Socket closed');
+	});
+
+	client.on('end', () => {
+		console.log('Socket ended');
+	});
+
+	setInterval(() => {
+		client.write('Hello World '+ new Date());
+	}, 1);
+});
---
+++ src/network/protocols/tcp/tcp-echo-server.ts
@@ -0,0 +1,20 @@
+import * as net from 'net';
+
+const server = net.createServer();
+
+server.on('connection', (socket) => {
+	socket.on('data', (data) => {
+		console.log("Received data", data.toString());
+		socket.write(`Echo:\n${data.toString()}`);
+	});
+	socket.on('error', (error) => {
+		console.error('Socket Error', error);
+	});
+	socket.on('close', () => {
+		console.log('Socket closed');
+	});
+});
+
+server.listen(8080, () => {
+	console.log('Server started');
+});
